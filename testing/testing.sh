b48b5b79eea4e5666eb80ec24123c3698d6aa428
NeodymiumFerBore <32781483+NeodymiumFerBore@users.noreply.github.com>
2024-03-05 00:38:22 +0100

    testing.sh: distinguish unset and empty arg

--- testing/testing.sh
+++ testing/testing.sh
@@ -8,6 +8,6 @@ case "$1" in
   1) echo " '[%C]'" >&2;              __kube_ps1 '[%C]' ;;
   2) echo " '%u@%c:%n'" >&2;          __kube_ps1 '%u@%c:%n' ;;
   3) echo " '\e[1;34m⎈|%n\e[0m'" >&2; __kube_ps1 '\e[1;34m⎈|%n\e[0m' ;;
-  *) [ -n "$1" ] && (echo " '$1'" >&2; __kube_ps1 "$1") || (echo >&2; __kube_ps1) ;;
+  *) [ -n "${1+x}" ] && (echo " '$1'" >&2; __kube_ps1 "$1") || (echo >&2; __kube_ps1) ;;
 esac
 echo
